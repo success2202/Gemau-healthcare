df271ce22af9107a8c0e43637aa460b8f5bce8d8
Michael Ozoudeh <jesmikky@gmail.com>
2023-12-13 22:44:03 +0100

    updated

--- resources/views/partials/mobile_nav.blade.php
+++ resources/views/partials/mobile_nav.blade.php
@@ -2,6 +2,6 @@
     <div class="ps-nav__item"><a href="#" id="open-menu"><i class="icon-menu"></i></a><a href="#" id="close-menu"><i class="icon-cross"></i></a></div>
     <div class="ps-nav__item"><a href="{{route('index')}}"><i class="icon-home2"></i></a></div>
     <div class="ps-nav__item"><a href="{{route('users.account.index')}}"><i class="icon-user"></i></a></div>
-    {{-- <div class="ps-nav__item"><a href="wishlist.html"><i class="fa fa-heart-o"></i><span class="badge"></span></a></div> --}}
     <div class="ps-nav__item"><a href="{{route('carts.index')}}"><i class="icon-cart-empty"></i><span class="badge cartReload">{{Cart::count()}}</span></a></div>
+        <div class="ps-nav__item"><a href=""><i class="fa fa-envelope-o"></i><span class="badge"></span></a></div>
 </div>
