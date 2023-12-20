2700c56dc43e4272044cfcaae6e6ebc8fa33ef3a
Michael Ozoudeh <jesmikky@gmail.com>
2023-12-20 11:17:14 +0100

    updated all

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="format-detection" content="telephone=no">
     <meta name="apple-mobile-web-app-capable" content="yes">
-    <link href="{{ asset('assets/images/'.$settings->fav)}}" rel="shortcut icon" type="image/png">
+    <link href="{{ asset('images/'.$settings->fav)}}" rel="shortcut icon" type="image/png">
     <meta name="csrf-token" content="{{ csrf_token() }}" />
     <meta name="author" content="Live Health epharm">
     <meta name="application-name" content="livehealthepharma Web-App">
