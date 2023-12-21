33429fc48d1000873637c5f35edf3a36169cdb97
Michael Ozoudeh <jesmikky@gmail.com>
2023-12-21 02:19:28 +0100

    updated the otp

--- resources/views/errors/419.blade.php
+++ resources/views/errors/419.blade.php
@@ -1,13 +1,12 @@
 @extends('layouts.app')
 @section('content')
 <div class="ps-page__content">
-    <div class="row">
-        <div class="col-12 col-md-6 col-lg-5"><img src="{{asset('frontend/img/cartoon-doctor.jpg')}}" alt="cartoon-doctor"></div>
-        <div class="col-12 col-md-6 col-lg-7">
-            <h1 class="ps-page__name">419</h1>
-            <h5>Something went wrong, please refresh page</h5>
-            <p>Please back to homepage</p>
-            <div><a class="ps-btn ps-btn--primary" href="{{route('users.index')}}">Back to homepage</a></div>
+    <div class="row p-5">
+        <div class="col-12 col-md-12 col-lg-12" style="display: block; text-align:center">
+            
+            <p>Something went wrong, please refresh page<p>
+            <a class="btn btn-success" style="border-radius: 5px" href="{{route('users.index')}}">Back to homepage</a>
+      
         </div>
     </div>
 </div>
