7123db0b57b1c869feb8d97325303f0f00c5d819
Michael Ozoudeh <jesmikky@gmail.com>
2023-12-27 11:51:06 +0100

    password reset fixed

--- resources/views/mails/orderMail.blade.php
+++ resources/views/mails/orderMail.blade.php
@@ -97,6 +97,14 @@
                                         </table>
 
 
+                                        <table style="width:100%;border:1px solid #ccc;margin-top:5px;table-layout:auto">
+                                            <tbody>
+                                          
+                                            <tr>
+                                                <td style="font-weight:bold!important;text-transform:uppercase!important">TOTAL</td>
+                                                <td style="text-align:right"> {{$data['amount']}}</td>
+                                            </tr>
+                                        </tbody></table>
 
                            
 
