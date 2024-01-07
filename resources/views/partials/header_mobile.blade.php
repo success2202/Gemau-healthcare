89586e4b3fba1d9f3bca9f7fa660ddc9fce8bda9
Michael Ozoudeh <jesmikky@gmail.com>
2024-01-07 21:36:54 +0100

    search button changed

--- resources/views/partials/header_mobile.blade.php
+++ resources/views/partials/header_mobile.blade.php
@@ -9,9 +9,23 @@
             <div class="ps-logo"><a href="{{route('users.index')}}"> <img src="{{asset('images/'.$settings->site_logo)}}" alt></a></div>
             <div class="ps-header__right">
                 <ul class="ps-header__icons">
-                    <li><a class="ps-header__item open-search" href="#"><i class="fa fa-search"></i></a></li>
+                    {{-- <a href="#" id="open-menu"><i class="icon-menu"></i></a><a href="#" id="close-menu"><i class="icon-cross"></i></a> --}}
                 </ul>
             </div>
         </div>
     </div>
+        <div class="ps-search__content ps-search--mobile" style="padding: 15px">
+            <form action="{{route('products.search')}}" method="get">
+                <div class="ps-search-table" style="border-radius: 5px">
+                    <div class="input-group">
+                        <input class="form-control ps-input" style="border-radius: 5px" name="q" type="text" placeholder="Search for products">
+                        <div class="input-group-append">
+                            <button class="btn " style="background:#07631d; color:#fff; padding:8px" type="submit"> 
+                                <span style="font-size: 15px"> Search </span></button></div>
+                    </div>
+                </div>
+            </form>
+  
+    </div>
+    
 </header>
