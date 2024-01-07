24fef3985e71b62ac765546aa1edd76729aaa77b
Michael Ozoudeh <jesmikky@gmail.com>
2024-01-07 05:17:50 +0100

    media

--- database/migrations/2020_06_14_000001_create_media_table.php
+++ database/migrations/2020_06_14_000001_create_media_table.php
@@ -8,15 +8,15 @@ class CreateMediaTable extends Migration
 {
     public function up()
     {
-        Schema::create('media', function (Blueprint $table) {
-            // $table->bigIncrements('id');
-            // $table->morphs('medially');
-            // $table->text('file_url');
-            // $table->string('file_name');
-            // $table->string('file_type')->nullable();
-            // $table->unsignedBigInteger('size');
-            // $table->timestamps();
-        });
+        // Schema::create('media', function (Blueprint $table) {
+        //     $table->bigIncrements('id');
+        //     $table->morphs('medially');
+        //     $table->text('file_url');
+        //     $table->string('file_name');
+        //     $table->string('file_type')->nullable();
+        //     $table->unsignedBigInteger('size');
+        //     $table->timestamps();
+        // });
     }
 
     /**
