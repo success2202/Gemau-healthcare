d54eaeec871fd26e3af284cfd52158e7ce9b360c
Michael Ozoudeh <jesmikky@gmail.com>
2025-01-13 09:41:47 +0100

    added google tags

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -48,11 +48,18 @@
         color:#000;
     }
 </style>
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-MSKX7LHR');</script>
+    <!-- End Google Tag Manager -->
 
     <!-- Google tag (gtag.js) -->
 <!--<script async src="https://www.googletagmanager.com/gtag/js?id=AW-11172431606"></script>-->
 <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-G8Q4Z4SFSQ">
+{{-- <script async src="https://www.googletagmanager.com/gtag/js?id=G-G8Q4Z4SFSQ">
     google-site-verification=C_cQyxouQa2uttVZE64GP2PeP7YnVzPWoonTiyvsebE
 </script>
 <script>
@@ -61,19 +68,23 @@
   gtag('js', new Date());
 
   gtag('config', 'G-G8Q4Z4SFSQ');
-</script>
+</script> --}}
 
 <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-0QPD7YQNP1"></script>
-<script>
+{{-- <script async src="https://www.googletagmanager.com/gtag/js?id=G-0QPD7YQNP1"></script> --}}
+{{-- <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
   gtag('config', 'G-0QPD7YQNP1');
-</script>
+</script> --}}
 </head>
 <body>
+    <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MSKX7LHR"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
     <div class="ps-page">
 
 @include('partials.header')
